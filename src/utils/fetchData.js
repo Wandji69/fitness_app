b616e7b6336c94aa086f55064575d3c9cd54c8e4
Wandji69 <collinschuwa@gmail.com>
2022-07-13 01:07:05 +0100

    rename of file

---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY
+    }
+  };
+
+export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+
+    return data;
+};
