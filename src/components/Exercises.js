d1c051590e8761565cc14ab38b41913a2915b708
Wandji69 <collinschuwa@gmail.com>
2023-09-07 17:32:22 +0100

    Added excesise results, to show different workouts, and their target muscles

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Pagination } from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { ExerciseCard } from "../components";
 
-const Exercises = () => {
-  return (
-    <div>Exercises</div>
-  )
-}
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+	console.log(exercises);
+	return (
+		<Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+			<Typography variant="h3" mb="46px">
+				Showing Results
+			</Typography>
+			<Stack
+				direction="row"
+				sx={{ gap: { lg: "110px", xs: "50px" } }}
+				flexWrap="wrap"
+				justifyContent="center"
+			>
+				{exercises.map((exercise, index) => (
+					<ExerciseCard key={index} exercise={exercise} />
+				))}
+			</Stack>
+		</Box>
+	);
+};
 
-export default Exercises
+export default Exercises;
